ecf9370c99c55ae327a5983fe57af70307458373
tktaofik <tktaofik@users.noreply.github.com>
2020-01-02 19:02:04 -0500

    Update app.py

--- services/job/app.py
+++ services/job/app.py
@@ -7,33 +7,20 @@ from service.routes import routes
 from service.db import init_db
 from service.config import get_config
 
+config = get_config()
 
-async def init_app(argv=None):
+
+async def init_app():
+    logging.basicConfig(level=logging.INFO)
 
     app = web.Application()
-
-    # save app config in app
-    app['config'] = get_config(argv)
-
-    # create db pool connection
-    await init_db(app)
-
-    # setup  routes
+    app['config'] = config
     app.add_routes(routes)
+
+    await init_db(app)
 
     return app
 
-
-def main(argv):
-    logging.basicConfig(level=logging.ERROR)
-
-    app = init_app(argv)
-
-    config = get_config(argv)
-    web.run_app(app,
-                host=config['host'],
-                port=config['port'])
-
-
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    app = init_app()
+    web.run_app(app, host=config['host'], port=config['port'])
